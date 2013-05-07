3d50ced0049df888d2ee1981e87233e2f7eba47f
Codemis <johnathan@missionaldigerati.com>
2013-05-07 13:16:17 -0700

    Initial Commit

---
+++ MilesDatas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MilesDatas
+//
+//  Created by Johnathan Pulos on 5/7/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDAppDelegate class]));
+    }
+}
