501e322638d60106daf452ebc678d3973c46904b
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-06-07 16:30:00 -0700

    Add helper methods to create location and add annotation to map

--- MilesDatas/MDRouteVC.m
+++ MilesDatas/MDRouteVC.m
@@ -10,21 +10,36 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
+-(MKPointAnnotation *)addPinToMapAtLocation:(CLLocationCoordinate2D)location
+                                  withTitle:(NSString *)title
+                               withSubtitle:(NSString *)subtitle {
+    MKPointAnnotation *point = MKPointAnnotation.new;
+    point.coordinate = location;
+    point.title = title;
+    point.subtitle = subtitle;
+    [self.mapView addAnnotation:point];
+    return point;
+}
+-(CLLocationCoordinate2D)locationWithLatitude:(double)latitude withLongitude:(double)longitude {
+    CLLocationCoordinate2D location;
+    location.latitude = latitude;
+    location.longitude = longitude;
+    return location;
+}
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:YES];
-    CLLocationCoordinate2D zoomLocation;
-    zoomLocation.latitude = [self.record[@"start_lat"] doubleValue];
-    zoomLocation.longitude = [self.record[@"start_long"] doubleValue];
+    CLLocationCoordinate2D startLocation =
+      [self locationWithLatitude:[self.record[@"start_lat"] doubleValue]
+                   withLongitude:[self.record[@"start_long"] doubleValue]];
     MKCoordinateRegion viewRegion =
       MKCoordinateRegionMakeWithDistance(
-        zoomLocation,0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+        startLocation,0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
     [self.mapView setRegion:[self.mapView regionThatFits:viewRegion]
                    animated:YES];
-    MKPointAnnotation *point = MKPointAnnotation.new;
-    point.coordinate = zoomLocation;
-    point.title = self.record[@"start_location"];
-    //TODO: format odometer
-    point.subtitle = [NSString stringWithFormat:@"Odometer: %@",self.record[@"start_odometer"]];
-    [self.mapView addAnnotation:point];
+    NSString *subtitle = [NSString stringWithFormat:@"Odometer: %@",self.record[@"start_odometer"]];
+    MKPointAnnotation *startPoint = [self addPinToMapAtLocation:startLocation
+                                                 withTitle:self.record[@"start_location"]
+                                              withSubtitle:subtitle];
+    [self.mapView selectAnnotation:startPoint animated:YES];
 }
 @end
