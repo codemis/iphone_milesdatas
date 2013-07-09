ce01a015cd3263379b9138f46f1c9e8e480ac4ac
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-07-09 14:28:08 -0700

    MapView rectangle includes start and stop locations

--- MilesDatas/MDRouteVC.m
+++ MilesDatas/MDRouteVC.m
@@ -1,15 +1,21 @@
 #import "MDRouteVC.h"
 #import <MapKit/MapKit.h>
-#define METERS_PER_MILE 1609.344
+#define EDGE_INSETS UIEdgeInsetsMake(20, 20, 20, 20)
+#define MAP_ANNOTATIONS_COUNT 2
+MKMapRect coordinateRegionForCoordinates(CLLocationCoordinate2D *coords,
+                                         NSUInteger coordCount) {
+    MKMapRect r = MKMapRectNull;
+    for (NSUInteger i=0; i<coordCount; ++i) {
+        MKMapPoint p = MKMapPointForCoordinate(coords[i]);
+        r = MKMapRectUnion(r, MKMapRectMake(p.x, p.y, 0, 0));
+    }
+    return r;
+}
 @interface MDRouteVC ()
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
 @implementation MDRouteVC
--(void)viewDidLoad {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
 -(MKPointAnnotation *)addPinToMapAtLocation:(CLLocationCoordinate2D)location
                                   withTitle:(NSString *)title
                                withSubtitle:(NSString *)subtitle {
@@ -20,7 +26,8 @@
     [self.mapView addAnnotation:point];
     return point;
 }
--(CLLocationCoordinate2D)locationWithLatitude:(double)latitude withLongitude:(double)longitude {
+-(CLLocationCoordinate2D)locationWithLatitude:(double)latitude
+                                withLongitude:(double)longitude {
     CLLocationCoordinate2D location;
     location.latitude = latitude;
     location.longitude = longitude;
@@ -29,32 +36,39 @@
 -(NSString *)formatOdometer:(NSNumber *)odometer {
     NSNumberFormatter *formatter = NSNumberFormatter.new;
     formatter.numberStyle = NSNumberFormatterDecimalStyle;
-    return [NSString stringWithFormat:@"Odometer: %@",[formatter stringFromNumber:odometer]];
+    return [NSString stringWithFormat:@"Odometer: %@",
+            [formatter stringFromNumber:odometer]];
 }
 -(NSNumber *)odometer:(NSString *)reading {
     return [NSNumber numberWithDouble:[reading doubleValue]];
 }
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:YES];
+    CLLocationCoordinate2D *coords = malloc(MAP_ANNOTATIONS_COUNT *
+                                            sizeof(CLLocationCoordinate2D));
     CLLocationCoordinate2D startLocation =
       [self locationWithLatitude:[self.record[@"start_lat"] doubleValue]
                    withLongitude:[self.record[@"start_long"] doubleValue]];
-    MKCoordinateRegion viewRegion =
-      MKCoordinateRegionMakeWithDistance(
-        startLocation,0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
-    [self.mapView setRegion:[self.mapView regionThatFits:viewRegion]
-                   animated:YES];
-    NSString *startSubtitle = [self formatOdometer:[self odometer:self.record[@"start_odometer"]]];
-    MKPointAnnotation *startPoint = [self addPinToMapAtLocation:startLocation
-                                                 withTitle:self.record[@"start_location"]
-                                              withSubtitle:startSubtitle];
+    coords[0] = startLocation;
+    NSString *startSubtitle =
+      [self formatOdometer:[self odometer:self.record[@"start_odometer"]]];
+    MKPointAnnotation *startPoint =
+      [self addPinToMapAtLocation:startLocation
+                        withTitle:self.record[@"start_location"]
+                     withSubtitle:startSubtitle];
     [self.mapView selectAnnotation:startPoint animated:YES];
     CLLocationCoordinate2D stopLocation =
     [self locationWithLatitude:[self.record[@"stop_lat"] doubleValue]
                  withLongitude:[self.record[@"stop_long"] doubleValue]];
-    NSString *stopSubtitle = [self formatOdometer:[self odometer:self.record[@"stop_odometer"]]];
+    coords[1] = stopLocation;
+    NSString *stopSubtitle =
+      [self formatOdometer:[self odometer:self.record[@"stop_odometer"]]];
     [self addPinToMapAtLocation:stopLocation
                       withTitle:self.record[@"stop_location"]
                    withSubtitle:stopSubtitle];
+    [self.mapView setVisibleMapRect:
+      coordinateRegionForCoordinates(coords,MAP_ANNOTATIONS_COUNT)
+                        edgePadding:EDGE_INSETS animated:NO];
+    free(coords);
 }
 @end
