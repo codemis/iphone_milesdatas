c637aca05df9031b4795a5151cc977863561baa1
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-09-02 04:31:03 -0700

    Improve consistency of names. Cosmetic.

--- MilesDatas/MDRouteVC.m
+++ MilesDatas/MDRouteVC.m
@@ -6,12 +6,12 @@
 @end
 
 @implementation MDRouteVC
--(void)addPinToMapAtLocation:(CLLocationCoordinate2D)location
-                   withTitle:(NSString *)title
-                withSubtitle:(NSString *)subtitle
-                    selected:(BOOL)selected{
+-(void)addPinToMapAtCoordinate:(CLLocationCoordinate2D)coordinate
+                     withTitle:(NSString *)title
+                  withSubtitle:(NSString *)subtitle
+                      selected:(BOOL)selected{
     MKPointAnnotation *point = MKPointAnnotation.new;
-    point.coordinate = location;
+    point.coordinate = coordinate;
     point.title = title;
     point.subtitle = subtitle;
     [self.mapView addAnnotation:point];
@@ -28,45 +28,47 @@
 }
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:YES];
+    self.mapView.showsUserLocation = NO;
     CLLocation *startLocation =
       [[CLLocation alloc] initWithLatitude:[self.record[@"start_lat"] doubleValue]
                                  longitude:[self.record[@"start_long"] doubleValue]];
     NSString *startSubtitle =
       [self formatOdometer:[self odometer:self.record[@"start_odometer"]]];
-    [self addPinToMapAtLocation:startLocation.coordinate
-                      withTitle:self.record[@"start_location"]
-                   withSubtitle:startSubtitle
-                       selected:YES];
+    [self addPinToMapAtCoordinate:startLocation.coordinate
+                        withTitle:self.record[@"start_location"]
+                     withSubtitle:startSubtitle
+                         selected:YES];
     CLLocation *stopLocation =
       [[CLLocation alloc] initWithLatitude:[self.record[@"stop_lat"] doubleValue]
                                  longitude:[self.record[@"stop_long"] doubleValue]];
     NSString *stopSubtitle =
       [self formatOdometer:[self odometer:self.record[@"stop_odometer"]]];
-    [self addPinToMapAtLocation:stopLocation.coordinate
-                      withTitle:self.record[@"stop_location"]
-                   withSubtitle:stopSubtitle
-                       selected:NO];
+    [self addPinToMapAtCoordinate:stopLocation.coordinate
+                        withTitle:self.record[@"stop_location"]
+                     withSubtitle:stopSubtitle
+                         selected:NO];
     [self.mapView showAnnotations:self.mapView.annotations animated:YES];
-    [self addOverlayForRouteFromLocation:startLocation toLocation:stopLocation];
+    [self addOverlayForRouteFromCoordinate:startLocation.coordinate
+                              toCoordinate:stopLocation.coordinate];
 }
 -(MKOverlayRenderer *)mapView:(MKMapView *)mapView
            rendererForOverlay:(id<MKOverlay>)overlay {
     if ([overlay isKindOfClass:[MKPolyline class]]) {
-        MKPolylineRenderer *renderer = [[MKPolylineRenderer alloc]
-                                        initWithPolyline:(MKPolyline *)overlay];
+        MKPolylineRenderer *renderer =
+          [[MKPolylineRenderer alloc] initWithPolyline:(MKPolyline *)overlay];
         renderer.strokeColor = [UIColor blueColor];
-        renderer.lineWidth = 1.0;
+        renderer.lineWidth = 3.0;
         return renderer;
     }
     return nil;
 }
--(void)addOverlayForRouteFromLocation:(CLLocation *)startLocation
-                           toLocation:(CLLocation *)stopLocation {
+-(void)addOverlayForRouteFromCoordinate:(CLLocationCoordinate2D)startCoordinate
+                           toCoordinate:(CLLocationCoordinate2D)stopCoordinate {
     MKPlacemark *startPlacemark =
-      [[MKPlacemark alloc] initWithCoordinate:startLocation.coordinate
+      [[MKPlacemark alloc] initWithCoordinate:startCoordinate
                             addressDictionary:nil];
     MKPlacemark *stopPlacemark =
-      [[MKPlacemark alloc] initWithCoordinate:stopLocation.coordinate
+      [[MKPlacemark alloc] initWithCoordinate:stopCoordinate
                             addressDictionary:nil];
     MKDirectionsRequest *directionsRequest = MKDirectionsRequest.new;
     directionsRequest.source =
